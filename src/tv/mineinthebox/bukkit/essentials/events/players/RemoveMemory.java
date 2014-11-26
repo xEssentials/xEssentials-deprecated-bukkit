a45a495c7d9aaaeab07979603ed986276d84afe7
xize <glucassen@live.nl>
2014-11-26 20:55:34 +0100

    added debug system on player leaving for xEssentialsPlayer and xEssentialsOfflinePlayer

--- src/tv/mineinthebox/bukkit/essentials/events/players/RemoveMemory.java
+++ src/tv/mineinthebox/bukkit/essentials/events/players/RemoveMemory.java
@@ -6,20 +6,34 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import tv.mineinthebox.bukkit.essentials.Configuration;
 import tv.mineinthebox.bukkit.essentials.xEssentials;
+import tv.mineinthebox.bukkit.essentials.enums.LogType;
 
 public class RemoveMemory implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onQuitMemory(PlayerQuitEvent e) {
+		if(Configuration.getDebugConfig().isEnabled()) {
+			xEssentials.getPlugin().log("preparing to remove player on normal quit." + e.getPlayer().getName(), LogType.DEBUG);
+		}
 		if(xEssentials.getManagers().getPlayerManager().isOnline(e.getPlayer().getName())) {
+			if(Configuration.getDebugConfig().isEnabled()) {
+				xEssentials.getPlugin().log("player found and removed!, check returns: " + xEssentials.getManagers().getPlayerManager().isOnline(e.getPlayer().getName()), LogType.DEBUG);
+			}
 			xEssentials.getManagers().getPlayerManager().removePlayer(e.getPlayer().getName());
 		}
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onQuitMemory(PlayerKickEvent e) {
+		if(Configuration.getDebugConfig().isEnabled()) {
+			xEssentials.getPlugin().log("preparing to remove player on normal kick." + e.getPlayer().getName(), LogType.DEBUG);
+		}
 		if(xEssentials.getManagers().getPlayerManager().isOnline(e.getPlayer().getName())) {
+			if(Configuration.getDebugConfig().isEnabled()) {
+				xEssentials.getPlugin().log("player found and removed!, check returns: " + xEssentials.getManagers().getPlayerManager().isOnline(e.getPlayer().getName()), LogType.DEBUG);
+			}
 			xEssentials.getManagers().getPlayerManager().removePlayer(e.getPlayer().getName());
 		}
 	}
