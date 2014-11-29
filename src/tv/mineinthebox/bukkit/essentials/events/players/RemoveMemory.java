0d6ccd79becba5dced7de1a6ebca18b59a937343
xize <glucassen@live.nl>
2014-11-29 15:19:04 +0100

    added more debug information about Kick and Quit being fired multiple times.

--- src/tv/mineinthebox/bukkit/essentials/events/players/RemoveMemory.java
+++ src/tv/mineinthebox/bukkit/essentials/events/players/RemoveMemory.java
@@ -1,5 +1,7 @@
 package tv.mineinthebox.bukkit.essentials.events.players;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,7 +14,7 @@ import tv.mineinthebox.bukkit.essentials.enums.LogType;
 
 public class RemoveMemory implements Listener {
 
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled=true)
 	public void onQuitMemory(PlayerQuitEvent e) {
 		if(Configuration.getDebugConfig().isEnabled()) {
 			xEssentials.getPlugin().log("preparing to remove player on normal quit: " + e.getPlayer().getName(), LogType.DEBUG);
@@ -21,6 +23,12 @@ public class RemoveMemory implements Listener {
 			xEssentials.getManagers().getPlayerManager().removePlayer(e.getPlayer().getName());
 			if(Configuration.getDebugConfig().isEnabled()) {
 				xEssentials.getPlugin().log("player found and removed!, online check returns: " + xEssentials.getManagers().getPlayerManager().isOnline(e.getPlayer().getName()), LogType.DEBUG);
+			}
+		} else {
+			if(Configuration.getDebugConfig().isEnabled()) {
+				xEssentials.getPlugin().log("player " + e.getPlayer().getName() + " was already removed due a kick event however this event shouldn't be fired see:", LogType.DEBUG);
+				xEssentials.getPlugin().log("found player in xEssentialsManager?: " + (xEssentials.getManagers().getPlayerManager().isOnline(e.getPlayer().getName()) ? ChatColor.GREEN + "yes" : ChatColor.RED + "no"), LogType.DEBUG);
+				xEssentials.getPlugin().log("player is still online by this event?: "+ (e.getPlayer() instanceof Player && e.getPlayer().isOnline() ? ChatColor.GREEN + "yes" : ChatColor.RED + "no"), LogType.DEBUG);
 			}
 		}
 	}
