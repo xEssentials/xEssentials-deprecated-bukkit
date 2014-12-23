0133bb9db3da45ec805344c83e8de809295f489c
xize <glucassen@live.nl>
2014-12-23 18:12:28 +0100

    update anti pwnage event.

--- src/tv/mineinthebox/bukkit/essentials/events/ban/PwnAgeProtectionEvent.java
+++ src/tv/mineinthebox/bukkit/essentials/events/ban/PwnAgeProtectionEvent.java
@@ -1,63 +1,62 @@
 package tv.mineinthebox.bukkit.essentials.events.ban;
 
-import java.util.HashMap;
-
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import tv.mineinthebox.bukkit.essentials.Configuration;
 import tv.mineinthebox.bukkit.essentials.xEssentials;
 import tv.mineinthebox.bukkit.essentials.interfaces.XPlayer;
 
 public class PwnAgeProtectionEvent implements Listener {
-
-	private HashMap<String, Long> time = new HashMap<String, Long>();
-	private HashMap<String, Long> time2 = new HashMap<String, Long>();
-
-	@EventHandler(priority = EventPriority.LOW)
+	
+	@EventHandler
 	public void playerJoinCheckSpamBot(PlayerJoinEvent e) {
-		XPlayer xp = xEssentials.getManagers().getPlayerManager().getPlayer(e.getPlayer().getName());	
-		if(!xp.isPermBanned() || !xp.isTempBanned()) {
-			if(time2.containsKey(e.getPlayer().getName())) {
-				if(System.currentTimeMillis() - time2.get(e.getPlayer().getName()) <= 1000) {
-				} else {
-					//relogged legit, setting default hashmap again.
-					time2.remove(e.getPlayer().getName());
-					time.put(e.getPlayer().getName(), System.currentTimeMillis());
-				}
-			}
-			if(time.containsKey(e.getPlayer().getName())) {
-				if(System.currentTimeMillis() - time.get(e.getPlayer().getName()) <= 1000) {
-					time.remove(e.getPlayer().getName());
-					time2.put(e.getPlayer().getName(), System.currentTimeMillis());
-					xp.setPermBanned(ChatColor.translateAlternateColorCodes('&', Configuration.getBanConfig().getPwnAgeSpamBanMessage()), "CONSOLE");
-					for(Player p : Bukkit.getOnlinePlayers()) {
-						XPlayer xpp = xEssentials.getManagers().getPlayerManager().getPlayer(p.getName());
-						if(xpp.isStaff()) {
-							p.sendMessage(ChatColor.RED + "player " + e.getPlayer().getName() + " is banned for botting!");
-						}
-						time2.remove(e.getPlayer().getName());
-					}
-				}
+		Player p = e.getPlayer();
+		final XPlayer xp = xEssentials.getManagers().getPlayerManager().getPlayer(p.getName());
+		
+		if(xp.hasLastLoginTime()) {
+			if((System.currentTimeMillis()-xp.getLastLoginTime()) > 280) {
+				xp.setLastLoginTime(System.currentTimeMillis());
 			} else {
-				//adding first hashmap, like any player, when hes putting in time2 he has bot behaviour, when its 3 hes gets banned.
-				//doing a check if this player is not in time2!
-				if(!time2.containsKey(e.getPlayer().getName())) {
-					time.put(e.getPlayer().getName(), System.currentTimeMillis());
+				if(xp.hasPwnageLevel()) {
+					if(xp.getPwnageLevel() > 2) {
+						xp.setPermBanned(Configuration.getBanConfig().getPwnAgeSpamBanMessage(), p.getName());
+					} else {
+						xp.setPwnageLevel(xp.getPwnageLevel()+1);
+						new BukkitRunnable() {
+
+							@Override
+							public void run() {
+								if(!xp.getPlayer().isBanned()) {
+									xp.setPwnageLevel(xp.getPwnageLevel()-1);
+								}
+							}
+							
+						}.runTaskLater(xEssentials.getPlugin(), 100);
+						xp.setLastLoginTime(System.currentTimeMillis());
+					}
+				} else {
+					xp.setPwnageLevel(xp.getPwnageLevel()+1);
+					new BukkitRunnable() {
+
+						@Override
+						public void run() {
+							if(!xp.getPlayer().isBanned()) {
+								xp.setPwnageLevel(xp.getPwnageLevel()-1);
+							}
+						}
+						
+					}.runTaskLater(xEssentials.getPlugin(), 100);
+					xp.setLastLoginTime(System.currentTimeMillis());
 				}
 			}
 		} else {
-			if(time.containsKey(e.getPlayer().getName())) {
-				time.remove(e.getPlayer().getName());
-			}
-			if(time2.containsKey(e.getPlayer().getName())) {
-				time2.remove(e.getPlayer().getName());
-			}
+			xp.setLastLoginTime(System.currentTimeMillis());
 		}
+		
+		
 	}
 }
