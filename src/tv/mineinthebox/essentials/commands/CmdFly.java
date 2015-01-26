d421e4da470f93db03dd9000a754ef1390ae0660
xize <glucassen@live.nl>
2015-01-26 16:01:54 +0100

    finish fly command so a admin is able to use /fly player

--- src/tv/mineinthebox/essentials/commands/CmdFly.java
+++ src/tv/mineinthebox/essentials/commands/CmdFly.java
@@ -11,34 +11,61 @@ import tv.mineinthebox.essentials.enums.PermissionKey;
 import tv.mineinthebox.essentials.interfaces.XPlayer;
 
 public class CmdFly {
-	
+
+	private final xEssentials pl;
+
+	public CmdFly(xEssentials pl) {
+		this.pl = pl;
+	}
+
 	public boolean execute(CommandSender sender, Command cmd, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("fly")) {
 			if(sender.hasPermission(PermissionKey.CMD_FLY.getPermission())) {
 				if(args.length == 0) {
 					if(sender instanceof Player) {
 						Player p = (Player) sender;
-						if(xEssentials.getManagers().getPlayerManager().isOnline(sender.getName())) {
-							XPlayer xp = xEssentials.getManagers().getPlayerManager().getPlayer(sender.getName());
+						if(pl.getManagers().getPlayerManager().isOnline(sender.getName())) {
+							XPlayer xp = pl.getManagers().getPlayerManager().getPlayer(sender.getName());
 							if(xp.isFlying()) {
 								xp.setFlying(false);
-								sender.sendMessage(ChatColor.GREEN + "successfully disabled fly mode");
+								sender.sendMessage(ChatColor.GRAY + "successfully disabled fly mode");
 								p.setFlying(false);
 								p.setAllowFlight(false);
 							} else {
 								xp.setFlying(true);
-								sender.sendMessage(ChatColor.GREEN + "successfully enabled fly mode");
+								sender.sendMessage(ChatColor.GRAY + "successfully enabled fly mode");
 								p.setAllowFlight(true);
 								p.setFlying(true);
 							}
 						} else {
-							sender.sendMessage(ChatColor.RED + "something went wrong, please reload xEssentials");
+							sender.sendMessage(ChatColor.RED + "something went wrong, please reload pl");
 						}
 					} else {
 						Warnings.getWarnings(sender).consoleMessage();
 					}
 				} else if(args.length == 1 ) {
-					
+					if(sender.hasPermission(PermissionKey.IS_ADMIN.getPermission())) {
+						if(pl.getManagers().getPlayerManager().isEssentialsPlayer(args[0])) {
+							if(pl.getManagers().getPlayerManager().isOnline(args[0])) {
+								XPlayer xp = pl.getManagers().getPlayerManager().getPlayer(args[0]);
+								if(xp.isFlying()) {
+									sender.sendMessage(ChatColor.GRAY + "you successfully disabled fly for player " + xp.getUser());
+									xp.getPlayer().sendMessage(ChatColor.GRAY + sender.getName() + " disabled your fly mode");
+									xp.setFlying(false);
+								} else {
+									sender.sendMessage(ChatColor.GRAY + "you successfully enabled fly for player " + xp.getUser());
+									xp.getPlayer().sendMessage(ChatColor.GRAY + sender.getName() + " enabled your fly mode");
+									xp.setFlying(true);
+								}
+							} else {
+								sender.sendMessage(ChatColor.RED + "player not online!");
+							}
+						} else {
+							sender.sendMessage(ChatColor.RED + "player does not exist!");
+						}
+					} else {
+						Warnings.getWarnings(sender).noPermission();
+					}
 				}
 			} else {
 				Warnings.getWarnings(sender).noPermission();
