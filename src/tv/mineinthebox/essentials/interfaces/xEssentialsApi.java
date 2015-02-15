737634af86eb9056f8b0243ef9192201a3c8d028
xize <glucassen@live.nl>
2015-02-15 17:21:34 +0100

    update javadocs for xEssentialsApi

--- src/tv/mineinthebox/essentials/interfaces/xEssentialsApi.java
+++ src/tv/mineinthebox/essentials/interfaces/xEssentialsApi.java
@@ -5,8 +5,20 @@ import tv.mineinthebox.essentials.managers.Manager;
 
 public interface xEssentialsApi {
 	
+	/**
+	 * returns the core managers of this plugin
+	 * 
+	 * @author xize
+	 * @return Manager
+	 */
 	public Manager getManagers();
 	
+	/**
+	 * returns the memory presentation from the configuration of this plugin
+	 * 
+	 * @author xize
+	 * @return Configuration
+	 */
 	public Configuration getConfiguration();
 	
 	
