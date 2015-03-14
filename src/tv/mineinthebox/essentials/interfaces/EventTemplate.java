ae380bb448b547f5277ac57802e34efed0e54e2c
xize <glucassen@live.nl>
2015-03-14 13:05:04 +0100

    redid javadoc for EventTemplate

--- src/tv/mineinthebox/essentials/interfaces/EventTemplate.java
+++ src/tv/mineinthebox/essentials/interfaces/EventTemplate.java
@@ -15,16 +15,33 @@ public class EventTemplate extends Template {
 		this.prefix = ChatColor.stripColor(prefix).toLowerCase();
 	}
 
+	/**
+	 * @author xize
+	 * @param p - the sender
+	 * @param message - the message which gets formatted
+	 */
 	public void sendMessage(CommandSender p, String message) {
 		String smsg = ChatColor.stripColor(message);
 		p.sendMessage(String.format(getPrefix(), prefix)+getSuffix()+smsg);
 	}
 	
+	/**
+	 * sents a message to a other player rather than the player inside the event
+	 * 
+	 * @author xize
+	 */
+	@Override
 	public void sendMessageTo(CommandSender p, String message) {
 		String smsg = ChatColor.stripColor(message);
 		p.sendMessage(String.format(getPrefix(), prefix)+getSuffix()+smsg);
 	}
 	
+	/**
+	 * broadcasts the message to every player by this event
+	 * 
+	 * @author xize
+	 */
+	@Override
 	public void broadcast(String message) {
 		String smsg = ChatColor.stripColor(message);
 		Bukkit.broadcastMessage(String.format(getPrefix(), prefix)+getSuffix()+smsg);
